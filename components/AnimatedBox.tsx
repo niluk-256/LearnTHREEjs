36794c3f3db2a0c21e344922db0f4c50f633d069
Neluka <nelukarulzz@gmail.com>
2022-11-16 12:42:53 +0530

    Orbit Controller removed

--- components/AnimatedBox.tsx
+++ components/AnimatedBox.tsx
@@ -1,18 +1,20 @@
 
+import { useHelper } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import { useRef } from "react"
-
+import { BoxHelper } from "three"
 const AnimationBox =() =>{
   const meshRef =useRef<THREE.Mesh>(null)
+  useHelper(meshRef,BoxHelper,"grey")
 useFrame(()=>{
     console.log("hi")
     if(meshRef.current){
 
-      meshRef.current.rotation.x += 0.02
+      meshRef.current.rotation.x += 0.01
     }
   })
   return(
-     <mesh ref={meshRef} scale ={[0.5,0.5,0.5]}>
+     <mesh visible={true} ref={meshRef} scale ={[0.5,0.5,0.5]}>
         <boxGeometry />
         <meshStandardMaterial/>
       </mesh>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import  type {NextPage} from "next"
-import { Canvas, ThreeElements } from "@react-three/fiber"
+import { Canvas, useThree} from "@react-three/fiber"
 import AnimationBox from "../components/AnimatedBox"
 
-
+import { OrbitControls , Stats } from "@react-three/drei"
 
 
 const Home: NextPage =() => {
-  
+  const testing = true
   return (
       <div className="container" >
      <Canvas>
+      <axesHelper visible={testing}/>
+      <OrbitControls/>
             <ambientLight intensity={0.1} />
   <directionalLight color="blue" position={[0, 0, 5]} />
      <AnimationBox/>
