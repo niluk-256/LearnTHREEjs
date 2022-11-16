d6487ff46b2a8018d9b19b418e663aff9e1403da
Neluka <nelukarulzz@gmail.com>
2022-11-16 12:07:10 +0530

    setup files

---
+++ components/AnimatedBox.tsx
@@ -0,0 +1,21 @@
+
+import { useFrame } from '@react-three/fiber'
+import { useRef } from "react"
+
+const AnimationBox =() =>{
+  const meshRef =useRef<THREE.Mesh>(null)
+useFrame(()=>{
+    console.log("hi")
+    if(meshRef.current){
+
+      meshRef.current.rotation.x += 0.02
+    }
+  })
+  return(
+     <mesh ref={meshRef} scale ={[0.5,0.5,0.5]}>
+        <boxGeometry />
+        <meshStandardMaterial/>
+      </mesh>
+  )
+}
+export default AnimationBox
