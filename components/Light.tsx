7a5741fef9e353ba0293f4e73e689e01394da8f4
Neluka <nelukarulzz@gmail.com>
2022-11-16 19:28:46 +0530

    lets go

--- components/Light.tsx
+++ components/Light.tsx
@@ -3,8 +3,8 @@ import { Canvas, useThree } from "@react-three/fiber"
 import { useHelper } from '@react-three/drei'
 import { DirectionalLightHelper } from 'three'
 function Light() {
-    const lightRef = useRef<THREE.DirectionalLight>()
-    useHelper(lightRef , DirectionalLightHelper , 5 , "gray")
+    // const lightRef = useRef<THREE.DirectionalLight>()
+    // useHelper(lightRef , DirectionalLightHelper , 5 , "gray")
   return (
     <><ambientLight intensity={0.1} />
     {/* ref={lightRef} */}
